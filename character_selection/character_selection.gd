de45b9af81d1b685d61c58969208eb0014d5bb8a
gingeredition <185860158+GingerEdition22@users.noreply.github.com>
2026-03-14 10:45:22 -0400

    Fixed layout

--- character_selection/character_selection.gd
+++ character_selection/character_selection.gd
@@ -5,8 +5,3 @@ func select_character(button):
 func _ready() -> void:
 	for button in $CharacterBox.get_children():
 		button.pressed.connect(select_character.bind(button))
-	print($CharacterBox/TextureButton.texture_normal)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
