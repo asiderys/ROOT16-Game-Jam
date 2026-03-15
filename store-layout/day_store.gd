c2c1baa396fd59a4667f6fbcd215bf947934f554
Arjun Siderys <146258439+asiderys@users.noreply.github.com>
2026-03-14 21:38:18 -0400

    pseudorandom NPCs added

--- store-layout/day_store.gd
+++ store-layout/day_store.gd
@@ -11,13 +11,15 @@ var testDict = {"Yellow": 1, "Orange": 0, "Pink": 1, "Green": 1}
 const orbNPC = preload("res://store-layout/orb.tscn")
 
 var npc_scene = preload("res://store-layout/orb.tscn")
-var orb = preload("res://store-layout/orb.tres")
+var orb = preload("res://store-layout/aliens/orb.tres")
 
-var npcTyepes = [ 
-	preload("res://store-layout/orb_yellow.tres"), 
-	preload("res://store-layout/orb_pink.tres"), 
-	preload("res://store-layout/orb_green.tres"), 
-	preload("res://store-layout/orb_blue.tres") 
+var npcTypes = [ 
+	preload("res://store-layout/aliens/GOOMI.tres"), 
+	preload("res://store-layout/aliens/jello.tres"), 
+	preload("res://store-layout/aliens/orb.tres"), 
+	preload("res://store-layout/aliens/PT.tres"), 
+	preload("res://store-layout/aliens/sausage.tres"),
+	preload("res://store-layout/aliens/Strange Steve.tres") 
 ]
 
 const EMPTY_BIN = preload("res://store-layout/empty_bin.tscn")
@@ -25,17 +27,23 @@ const YELLOW = preload("res://store-layout/yellow_bin.tscn")
 const ORANGE = preload("res://store-layout/orange_bin.tscn")
 const PINK = preload("res://store-layout/pink_bin.tscn")
 const GREEN = preload("res://store-layout/green_bin.tscn")
+const NUM_NPCS = 5 # how many NPCs we want to spawn
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawn_npc(orb)
+	#spawn_npc(orb)
 	spawn_bins()
+	await get_tree().create_timer(5.0).timeout # just wait a few seconds before spawning any NPCs
+	for i in range (NUM_NPCS):
+		random_npc()
+		await get_tree().create_timer(20.0).timeout 
 
 func spawn_npc(frames):
 	var path = get_node("Path2D")
 	
 	pathFollow2D = PathFollow2D.new()
 	pathFollow2D.rotates = false
+	pathFollow2D.loop = false
 	path.add_child(pathFollow2D)
 	npc = orbNPC.instantiate()
 	pathFollow2D.add_child(npc)
@@ -47,6 +55,23 @@ func spawn_npc(frames):
 	var remote = RemoteTransform2D.new()
 	pathFollow2D.add_child(remote)
 	remote.remote_path = npc.get_path()
+
+func random_npc():
+	var randNPC = npcTypes.pick_random()
+	pathFollow2D = PathFollow2D.new()
+	pathFollow2D.rotates = false
+	var npc = npc_scene.instantiate()
+	pathFollow2D.add_child(npc)
+	get_node("Path2D").add_child(pathFollow2D)
+	# adding random sprite here instead of the green orb thing
+	var randSprite = npc.get_node("CharacterBody2D/AnimatedSprite2D")
+	randSprite.sprite_frames = randNPC
+	randSprite.play("motion")
+	
+	var remote = RemoteTransform2D.new()
+	pathFollow2D.add_child(remote)
+	remote.remote_path = npc.get_path()
+	
 
 func start_wait(loc, i):
 	isWaiting = true
