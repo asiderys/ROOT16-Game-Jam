2026a916a015e2440c21e6a388eec3786b3fcdfa
gingeredition <185860158+GingerEdition22@users.noreply.github.com>
2026-03-15 00:27:01 -0400

    Added sound effects

--- spaceship_selection/spaceship_selection.gd
+++ spaceship_selection/spaceship_selection.gd
@@ -6,13 +6,16 @@ func _ready() -> void:
 var colors = ["#FF0000", "#E57A10", "#FFFF00", "#00FF00", "#0000FF", "#FF00FF"]
 var current_hue = 0
 func next_sprite():
+	$DefaultButton.play()
 	current_hue = (current_hue + 1) % len(colors)
 	$SelectedVBox/SelectedHBox/Selected.set_modulate(colors[current_hue])
 func prev_sprite():
+	$DefaultButton.play()
 	current_hue = (current_hue - 1) % len(colors)
 	$SelectedVBox/SelectedHBox/Selected.set_modulate(colors[current_hue])
 
 
 func _on_continue_btn_pressed() -> void:
-	global.music_pos = $CharacterSelectionMusic.get_playback_position()
-	get_tree().change_scene_to_file("res://spaceship_selection/spaceship_selection.tscn")
+	$SpaceshipSelect.play()
+	await $SpaceshipSelect.finished
+	# TODO: CHANGE SCENE
