0b4fac008f1597d59a28419ced78bec83141f296
gingeredition <185860158+GingerEdition22@users.noreply.github.com>
2026-03-15 11:39:04 -0400

    Fixed scaling

--- character_selection/character_selection.gd
+++ character_selection/character_selection.gd
@@ -15,19 +15,23 @@ func _ready() -> void:
 	$SelectedVBox/SelectedHBox/Selected.sprite_frames = load("res://art/aliens/" + aliens[current_alien] + "/" + aliens[current_alien] + "_frames.tres")
 	$SelectedVBox/SelectedHBox/Selected.play()
 func next_sprite():
-	$DefaultButton.play()
 	current_alien = (current_alien + 1) % len(aliens)
-	$SelectedVBox/SelectedHBox/Selected.sprite_frames = load("res://art/aliens/" + aliens[current_alien] + "/" + aliens[current_alien] + "_frames.tres")
-	$SelectedVBox/SelectedHBox/Selected.play()
-	global.player_sprite = $SelectedVBox/SelectedHBox/Selected.sprite_frames
+	change_sprite()
 func prev_sprite():
-	$DefaultButton.play()
 	current_alien = (current_alien - 1) % len(aliens)
+	change_sprite()
+
+func change_sprite():
+	$DefaultButton.play()
 	$SelectedVBox/SelectedHBox/Selected.sprite_frames = load("res://art/aliens/" + aliens[current_alien] + "/" + aliens[current_alien] + "_frames.tres")
 	$SelectedVBox/SelectedHBox/Selected.play()
 	global.player_sprite = $SelectedVBox/SelectedHBox/Selected.sprite_frames
-
-
+	if "sausage" == aliens[current_alien]:
+		$SelectedVBox/SelectedHBox/Selected.scale = Vector2(0.15, 0.15)
+	elif "strange_steve" == aliens[current_alien]:
+		$SelectedVBox/SelectedHBox/Selected.scale = Vector2(0.25, 0.25)
+	else:
+		$SelectedVBox/SelectedHBox/Selected.scale = Vector2(0.218, 0.218)
 func _on_continue_btn_pressed() -> void:
 	$ChooseCharacter.play()
 	await $ChooseCharacter.finished
